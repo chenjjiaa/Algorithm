2cffdc796ba6c16479dd8485216f5c5be2f54aa7
励志买套上海苏河湾大平层 <94773058+chenjjiaa@users.noreply.github.com>
2022-11-16 00:23:55 +0800

    Add files via upload

---
+++ src/com/chenjjia/算法/LeetCode/A556_下一个更大元素III.java
@@ -0,0 +1,18 @@
+package com.chenjjia.算法.LeetCode;
+
+/**
+ * Author：  chenjunjia
+ * Date：    2022/11/15 22:23
+ * WeChat：  China_JoJo_
+ * Blog：    https://juejin.cn/user/1856417285289304/posts
+ * Github：  https://github.com/chenjjiaa
+ */
+
+/**
+ * 感觉和下一个排列有点像
+ */
+public class A556_下一个更大元素III {
+    public static void main(String[] args) {
+
+    }
+}
